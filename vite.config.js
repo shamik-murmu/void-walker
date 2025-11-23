8d2505ae631bc0e503031de69b3d2c17b6a35921
shamik-murmu <shamikmurmu@gmail.com>
2025-11-22 22:22:44 -0500

    Configure GitHub Actions for Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
     return {
+        base: '/void-walker/',
         server: {
             port: 3000,
             host: '0.0.0.0',
